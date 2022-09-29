bf77ea2a62f2725ac04f8052431ce920a3424a8e
afshintrg <ataraghijoo@gmial.com>
2022-09-29 11:12:13 +0330

    Add HomeView detail -show categories -show products

--- cart/order/models.py
+++ cart/order/models.py
@@ -21,12 +21,16 @@ class Product(models.Model):
     category = models.ManyToManyField(Category, related_name='products')
     name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=200, unique=True)
-    image = models.ImageField()
+    image = models.ImageField(null=True, blank=True)
     desc = models.TextField()
+    price = models.IntegerField()
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f'{self.name}'
+
+
+
 
--- cart/order/views.py
+++ cart/order/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Product, Category
 
 
 class HomeView(View):
     def get(self, request):
-        return render(request, 'order/home.html')
+        products = Product.objects.filter(available=True)
+        categories = Category.objects.filter(is_sub=False)
+        return render(request, 'order/home.html', {'products': products, 'categories': categories})
 
