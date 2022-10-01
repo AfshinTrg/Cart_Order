94ed80e09cfc8c252664c678d5840781942b2931
afshintrg <ataraghijoo@gmial.com>
2022-10-01 09:49:15 +0330

    Add total price feature for cart

--- cart/order/cart.py
+++ cart/order/cart.py
@@ -38,3 +38,6 @@ class Cart:
             del self.cart[p_id]
             self.save()
 
+    def get_total_price(self):
+        return sum(int(item['price']) * item['quantity'] for item in self.cart.values())
+
