5da834ed237755fde1aa02a814b1042844cf4b74
afshintrg <ataraghijoo@gmial.com>
2022-10-01 09:53:13 +0330

    Add cart counter feature and edit total price(cart.html)

--- cart/order/cart.py
+++ cart/order/cart.py
@@ -41,3 +41,6 @@ class Cart:
     def get_total_price(self):
         return sum(int(item['price']) * item['quantity'] for item in self.cart.values())
 
+    def __len__(self):
+        return sum(item['quantity'] for item in self.cart.values())
+
