444c0287d395c6dee693ebce3c48342a506524fb
afshintrg <ataraghijoo@gmial.com>
2022-10-01 10:24:03 +0330

    Add order model

--- cart/order/admin.py
+++ cart/order/admin.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
 from .models import Category, Product
-
+from .models import Order, OrderItem
 
 admin.site.register(Category)
 
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-	raw_id_fields = ('category',)
+    raw_id_fields = ('category',)
 
+
+class OrderItemInLine(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ('product',)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'updated', 'paid')
+    list_filter = ('paid',)
+    inlines = (OrderItemInLine,)
